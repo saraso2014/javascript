6f8926acd44440013f8db2416ee21643e50b2af1
Sara Soueidan <saraesoueidan@gmail.com>
2020-10-10 21:28:47 -0400

    javascript tutorial 10/10 complete

--- app/js/index.js
+++ app/js/index.js
@@ -4,5 +4,53 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Title')
-/* coding examples */
+
+logTitle('Variables')
+/* Variables */
+var name = "Maria Jones";
+var age = 21;
+var hasDriverLicense = true;
+var empty = undefined;
+
+/* Objects */
+// a collection of grouped variables
+var person = {
+    name: "Maria Jones",
+    age: 21,
+    hasDriverLicense: true,
+    dateOfBirth: "01/01/1999",
+    address: {
+        firstLine: "123",
+        postCode: "SE1",
+        country: "England",
+    }
+};
+
+
+log(JSON.stringify(person));
+
+log(person.name);
+log(person.age);
+log(person.hasDriverLicense);
+log(JSON.stringify(person.address));
+log(Object.values(person));
+
+// Var - single
+var name = "Alex";
+log(name);
+
+// Var - array
+var names = ["Sara","Alex","Hannah"];
+log(names);
+log(names[2]);
+log(names.length);
+
+// Loop through arrays - way 1
+for (var n of names) {
+    log(n);
+}
+
+// Loop through arrays - way 2
+names.forEach(function(n, index) {
+    log(index + " - " + n);
+})
